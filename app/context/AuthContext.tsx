c488362563edd3ec63c3ac2218d4f365979d76dd
mateusbrbza <mateus.c.barboza@gmail.com>
2023-05-23 11:41:18 -0300

    fixing login problem

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -2,10 +2,12 @@
 
 import { SessionProvider } from "next-auth/react";
 
-interface AuthContextProps {
-    children: React.ReactNode;
+export interface AuthContextProps {
+  children: React.ReactNode;
 }
 
-export default function AuthContext({ children }: AuthContextProps) {
-    return <SessionProvider>{children}</SessionProvider>
+export default function AuthContext({ 
+  children
+}: AuthContextProps) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
